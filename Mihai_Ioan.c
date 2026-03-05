8062664915c3df922b5bfc512cc9fbbc156c3388
Mihai Ioan <in.whapp@gmail.com>
2026-03-05 11:30:19 +0200

    final

--- Mihai_Ioan.c
+++ Mihai_Ioan.c
@@ -1,20 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+int function(int x, int *i)
+{
+    x = x - 2;
+    (*i)++;
+    if (x <= 0)
+        return x;
+    x = function(x, i);
 }
 
-int main(){
-	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
-	return 0;	
-}
+int main()
+{
+    int a = 30, i = 0;
+    a = function(a, &i);
 
+    printf("%d", i);
+    return 0;
+}
